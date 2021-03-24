365eec6f3409faf12f9a56abc0bec9799632eb50
Ali Alpay <alialpay96@hotmail.com>
2021-03-24 21:40:38 +0300

    20. ders sonu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,7 +15,8 @@ import { FilterPipePipe } from './pipes/filter-pipe.pipe';
 
 import {ToastrModule} from "ngx-toastr";
 import { CartSummaryComponent } from './components/cart-summary/cart-summary.component';
-import { ProductAddComponent } from './components/product-add/product-add.component'
+import { ProductAddComponent } from './components/product-add/product-add.component';
+import { LoginComponent } from './components/login/login.component'
 
 @NgModule({
   declarations: [
@@ -28,6 +29,7 @@ import { ProductAddComponent } from './components/product-add/product-add.compon
     FilterPipePipe,
     CartSummaryComponent,
     ProductAddComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
